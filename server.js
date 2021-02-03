d161f61446888092539ed0b68f182ca1be18b6f3
Taideptrai <nguyenvantaizzz@gmail.com>
2021-02-03 20:24:34 +0200

    check

--- server.js
+++ server.js
@@ -127,9 +127,9 @@ app.post(
 );
 const __dirname = path.resolve();
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
   )}
 
 app.listen(port, () =>{
