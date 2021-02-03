0569f20d874050ccf9ff737699ce62c5d8d6c1d7
Taideptrai <nguyenvantaizzz@gmail.com>
2021-02-03 06:38:47 +0200

    a

--- server.js
+++ server.js
@@ -23,13 +23,6 @@ app.use(function (req, res, next) {
   });
 
 ///serve static file assets if in production
-if (process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  });
-}
 
 app.get('/',(req, res)=>{
     res.send('server is ready');
