cb08c347594bd825ce4741368fe68b219246946f
Taideptrai <nguyenvantaizzz@gmail.com>
2021-02-03 20:14:00 +0200

    first

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-}
+  )}
 
 app.listen(port, () =>{
     console.log('server is running');
