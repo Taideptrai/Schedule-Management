dc1bb009ae4ae42504baec61c8c85e637af2049d
Taideptrai <nguyenvantaizzz@gmail.com>
2021-02-03 20:31:36 +0200

    make it better

--- server.js
+++ server.js
@@ -125,12 +125,13 @@ app.post(
     });
   }
 );
-const __dirname = path.resolve();
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/client/build/index.html'))
-  )}
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+  })
+  }
 
 app.listen(port, () =>{
     console.log('server is running');
